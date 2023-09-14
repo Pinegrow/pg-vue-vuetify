76ce7da2e4c0c51493525bea8ef4d6787f5427d9
Akayy <techakayy@gmail.com>
2023-09-14 16:43:42 +1000

    Updated pinegrow plugins/modules

--- src/site.ts
+++ src/site.ts
@@ -2,8 +2,8 @@ export default {
   title: `Vue Designer`,
   description: 'Vue Designer Vuetify - Quick start template',
   author: 'Pinegrow',
-  // nav: [
-  //   { text: 'Home', link: '/' },
-  //   { text: 'About', link: '/about' },
-  // ],
+  nav: [
+    { text: 'Home', link: '/' },
+    { text: 'About', link: '/about' },
+  ],
 }
